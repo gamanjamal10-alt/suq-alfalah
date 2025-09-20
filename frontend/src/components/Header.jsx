d8ebde987c72a0086de9b67fc1903c371324a3c8
gamanjamal10-alt <gamanjamal10@gmail.com>
2025-09-20 01:46:30 -1000

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header>
-      <nav>
-        <ul>
-          <li><Link to="/">🏠 Home</Link></li>
-          <li><Link to="/products">🛒 Products</Link></li>
-          <li><Link to="/about">ℹ️ About</Link></li>
-          <li><Link to="/contact">📞 Contact</Link></li>
-        </ul>
-      </nav>
+      <h1>سوق الفلاح</h1>
     </header>
   );
 }
