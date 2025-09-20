d07cd437903251fc0e1fe2607b828ba82a691d92
gamanjamal10-alt <gamanjamal10@gmail.com>
2025-09-20 00:17:34 -1000

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Products from "./pages/Products";
-import CreateStore from "./pages/CreateStore";
-import StorePage from "./pages/StorePage";
-import Support from "./pages/Support";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Header />
-      <main>
+      <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
-          <Route path="/create-store" element={<CreateStore />} />
-          <Route path="/store/:id" element={<StorePage />} />
-          <Route path="/support" element={<Support />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
-      </main>
-      <Footer />
-    </Router>
+      </div>
+    </BrowserRouter>
   );
 }
 
