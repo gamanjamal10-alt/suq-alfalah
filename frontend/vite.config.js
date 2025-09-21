e829e626de0c8f073f217e7f4590c07c973917a7
gamanjamal10-alt <gamanjamal10@gmail.com>
2025-09-21 10:55:30 -1000

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173, proxy: {'/api': { target: process.env.VITE_API_URL || 'http://localhost:5000', changeOrigin: true, secure: false }}},
-  build: { outDir: 'dist' }
+  server: {
+    proxy: {
+      "/api": "http://localhost:5000"
+    }
+  }
 });
