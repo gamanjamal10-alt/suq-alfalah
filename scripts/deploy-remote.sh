a8ef0c092c627de2a6423b811a85dd49e0f7d741
gamanjamal10-alt <gamanjamal10@gmail.com>
2025-09-21 11:34:15 -1000

    Delete scripts directory

--- scripts/deploy-remote.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-REMOTE=$1
-USER=${2:-root}
-if [ -z "$REMOTE" ]; then echo "Usage: deploy-remote.sh <host> [user]"; exit 1; fi
-ssh $USER@$REMOTE 'cd /opt/suq-alfalah && docker compose pull && docker compose up -d --remove-orphans'
