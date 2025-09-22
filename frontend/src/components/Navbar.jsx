f1be4f10d12bda3d17f03e497429406cab3d8e8e
gamanjamal10-alt <gamanjamal10@gmail.com>
2025-09-22 13:00:49 -1000

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from "react";
-
 export default function Navbar() {
   return (
-    <nav style={{ padding: "10px", background: "#eee" }}>
+    <nav className="navbar">
       <h2>سوق الفلاح</h2>
+      <ul>
+        <li>الرئيسية</li>
+        <li>المتاجر</li>
+        <li>المنتجات</li>
+        <li>اتصل بنا</li>
+      </ul>
     </nav>
   );
 }
